1b0020dbbc4459ec6c583c517e16ecd3ccba71b3
Subhodeep Bandyopadhyay <sb@beon.net>
2023-03-08 22:26:25 +0000

    080323

--- ui5/sampleapp/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["sap/btp/sampleapp/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- ui5/sampleapp/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"sapbtp/sampleapp/test/unit/controller/Sampleui5app.controller"
-], function () {
-	"use strict";
-});
